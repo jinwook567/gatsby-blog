371f7377d248b7847abfdd41ffccb0bd36fc11de
jinwook567 <jinwook567@we2d.com>
2022-12-26 15:49:59 +0900

    feat: gatsby-my-config 파일 추가, manifest 설정

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,31 +1,29 @@
 import type { GatsbyConfig } from 'gatsby';
+import myConfig from './gatsby-my-config';
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `gatsby-blog`,
-    siteUrl: `https://www.yourdomain.tld`,
-    author: {
-      name: 'jinwook',
-      description: 'This is my blog',
-      social: {
-        github: 'https://github.com/jinwook567',
-        email: 'dlwlsdnr567@naver.com',
-        instagram: '',
-      },
-    },
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
+  siteMetadata: myConfig.siteMetadata,
   graphqlTypegen: true,
   plugins: [
     'gatsby-plugin-emotion',
-    'gatsby-plugin-google-gtag',
+    {
+      resolve: 'gatsby-plugin-google-gtag',
+      options: {
+        trackingIds: myConfig.ga.trackingIds,
+        pluginConfig: {
+          head: true,
+        },
+      },
+    },
     'gatsby-plugin-image',
     'gatsby-plugin-sitemap',
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
+        name: myConfig.siteMetadata.title,
+        short_name: myConfig.siteMetadata.title,
+        start_url: '/',
+        display: 'standalone',
         icon: 'src/images/icon.png',
       },
     },
---
+++ gatsby-my-config.ts
@@ -0,0 +1,20 @@
+const config = {
+  siteMetadata: {
+    title: `gatsby-blog`,
+    siteUrl: 'https://www.yourdomain.tld',
+    author: {
+      name: 'jinwook',
+      description: 'This is my blog',
+      social: {
+        github: 'https://github.com/jinwook567',
+        email: 'dlwlsdnr567@naver.com',
+        instagram: '',
+      },
+    },
+  },
+  ga: {
+    trackingIds: [],
+  },
+};
+
+export default config;
