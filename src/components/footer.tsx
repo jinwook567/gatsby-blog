91822d08cca8901e79ed761bc95e6011514cc842
jinwook567 <jinwook567@we2d.com>
2022-12-27 12:56:22 +0900

    fix: footer github link 추가

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Grid } from '@mui/material';
+import styled from '@emotion/styled';
 import { Author } from '../../types';
 
 type Props = {
   author: Author;
 };
 
+const StyledLink = styled.a`
+  text-decoration: underline;
+`;
+
 function Footer({ author }: Props) {
   const year = new Date().getFullYear();
-  return <Grid>{`© ${year} ${author.name}, Built with Gatsby`}</Grid>;
+  return (
+    <Grid>
+      {`© ${year} ${author.name}, Powered by `}
+      <StyledLink href="https://github.com/jinwook567/gatsby-blog">
+        gatsby-blog
+      </StyledLink>
+    </Grid>
+  );
 }
 
 export default Footer;
